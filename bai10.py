85e55cb3727e0b422b2fa81607e15f731d00ef34
LeHuynhSaoMai <lhsmai1411@gmail.com>
2024-09-02 20:45:08 +0700

    Add files via upload

--- bai10.py
+++ bai10.py
@@ -6,5 +6,5 @@ Created on Mon Sep  2 18:19:49 2024
 """
 
 a = int(input("Số xe (gồm 4 chữ số) của bạn: "))
-var = str(a)  # Chuyển số nguyên thành chuỗi để truy cập từng chữ số
+var = str(a) 
 print("Số nút là:", int(var[0]) + int(var[1]) + int(var[2]) + int(var[3]))
---
+++ bai25.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 20:10:03 2024
+
+@author: 
+"""
+a = input("Hãy nhập một chữ cái: ")
+if a.islower():
+    print("Ký tự chữ hoa:", a.upper())
+else:
+    print("Ký tự chữ thường:", a.lower())
