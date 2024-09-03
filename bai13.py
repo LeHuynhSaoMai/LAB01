688ca727d471cf322e70a82c7bd7d66c8194c9fd
LeHuynhSaoMai <lhsmai1411@gmail.com>
2024-09-03 19:02:14 +0700

    Add files via upload

--- bai13.py
+++ bai13.py
@@ -1,41 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep  3 00:29:38 2024
+Created on Tue Sep  3 18:42:46 2024
 
-@author: Smai
+@author: User
 """
 
-h1 = int(input("Nhập giờ thứ nhất: "))
-m1 = int(input("Nhập phút thứ nhất: "))
-s1 = int(input("Nhập giây thứ nhất: "))
-
-h2 = int(input("Nhập giờ thứ hai: "))
-m2 = int(input("Nhập phút thứ hai: "))
-s2 = int(input("Nhập giây thứ hai: "))
-
-if h1 > 24 and m1 > 60 and s1 > 60:
-    print("Thời gian không hợp lệ")
-if h2 > 24 and m2 > 60 and s2 > 60:
-    print("Thời gian không hợp lệ")
-    
-tonggiayh1 = h1 * 3600
-tonggiaym1 = m1 * 60
-tong1 = tonggiayh1 + tonggiaym1 + s1
-
-tonggiayh2 = h2 * 3600
-tonggiaym2 = m2 * 60
-tong2 = tonggiayh2 + tonggiaym2 + s2
-
-if tong1 > tong2:
-    ketqua = tong1 - tong2
-    gioketqua = ketqua // 3600
-    phutketqua = (ketqua % 3600) // 60
-    giayketqua = (ketqua % 3600) % 60
-    print("Kết quả: ",gioketqua, "giờ", phutketqua, "phút", giayketqua,"giây")
-    
+a = int(input("Nhập ngày: "))
+b = int(input("Nhập tháng: "))
+c = int(input("Nhập năm: "))
+if 1<=a<=31 and 1<=b<=12 and 1000<=c:
+# Câu a
+    a=str(a)
+    b=str(b)
+    c=str(c)
+    print(a,"/",b,"/",c)
+# Câu b
+    print(a, "/", b, "/", c[2:4])
+# Câu c
+    print(c, "/", b, "/",a)
 else:
-    ketqua = tong2 - tong1
-    gioketqua = ketqua // 3600
-    phutketqua = (ketqua % 3600) // 60
-    giayketqua = (ketqua % 3600) % 60
-    print("Kết quả: ",gioketqua, "giờ", phutketqua, "phút", giayketqua,"giây")
+    print("Lỗi! Nhập sai")
